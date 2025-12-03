100d77030b6ca75ba6d01384cb45c003fea7122a
menes000 <menes_arslan@hotmail.com>
2025-12-03 10:28:08 +0300

    basic ems structure

--- ems.c
+++ ems.c
@@ -1,13 +1,47 @@
 #include <stdtio.h>
 
-struct node{
+struct EmployeeNode{
+	int employee_id;
+	char name[50];
+	float salary;
+	struct EmployeeNode* next;
+}
+
+//listenin basina yeni employee ekler ve listenin yeni head ini dondurur
+struct EmployeeNode* insertEmployee(){
+	
 
 
 }
 
+//secilen employee yi siler ve listenin head ini dondurur
+struct EmployeeNode* deleteEmployee(){
+
+
+}
+
+//secilen employeenin magasini istenilen degerle gunceller returns 1 or 0
+int searchAndUpdateSalary(){
+
+
+}
+
+//butun employye listesini konsola yazdirir
+void displayAllEmployees(){
+
+
+}
+
+//deallacotes everything
+void freeList(){
+
+}
+
+
 int main (){
-
-
+	struct EmployeeNode* head = malloc(sizeof(struct EmployeeNode));
+	
+	
 
 
 
